a3386cba1db2aabeeeac05418590f4fcd142d10c
Conrad Foik <ge54huz@foik.eda.cit.tum.de>
2024-10-31 15:42:41 +0100

    Updating environment description and adding M2ISAR to setup script

--- scripts/setup_workspace.sh
+++ scripts/setup_workspace.sh
@@ -8,6 +8,13 @@ set -e
 # Setup etiss-perf-sim
 ${PSW_PERF_SIM}/setup_simulator.sh
 
+# Setup M2-ISA-R
+cd ${PSW_M2ISAR}
+python3 -m venv venv
+source venv/bin/activate
+pip install -e .
+deactivate
+
 # Setup M2-ISA-R-Perf
 cd ${PSW_M2ISAR_PERF}
 python3 -m venv venv
